5c10634e4729814de1ccafccd8bb405a7fae2544
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2025-05-16 22:18:49 +0200

    Init with filter method and tests

---
+++ src/Iterable.hpp
@@ -0,0 +1,23 @@
+#ifndef ITERABLE_HPP
+#define ITERABLE_HPP
+
+#include <list>
+
+template<typename T>
+using predicate_t = std::function<bool(const T&)>;
+
+template<typename T>
+std::list<T> filter(std::list<T> list, const predicate_t<T>& predicate) {
+    std::list<T> result;
+    for (const auto& item : list)
+    {
+        if (predicate(item))
+        {
+            result.push_back(item);
+        }
+    }
+
+    return result;
+}
+
+#endif // ITERABLE_HPP
