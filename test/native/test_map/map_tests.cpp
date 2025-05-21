23ddf34c2b945567b097349bcee519044f4b16d8
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2025-05-21 23:30:19 +0200

    Added class based test for map method

--- test/native/test_map/map_tests.cpp
+++ test/native/test_map/map_tests.cpp
@@ -4,7 +4,8 @@
 #include "iterable.hpp"
 #include "unity.h"
 #include <string>
-
+#include "animal.hpp"
+#include "pet.hpp"
 
 void setUp()
 {
@@ -16,10 +17,12 @@ void tearDown()
     // clean stuff up here
 }
 
-void map_test() {
+void map_listOfStrings_listOfSubstrings()
+{
     std::list<std::string> list = {"aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"};
 
-    list = map<std::string, std::string>(list, [](const std::string& s) { return s.substr(0, 3); });
+    list = map<std::string, std::string>(list, [](const std::string &s)
+                                         { return s.substr(0, 3); });
 
     TEST_ASSERT_EQUAL_STRING("aaa", list.front().c_str());
     list.pop_front();
@@ -34,10 +37,24 @@ void map_test() {
     TEST_ASSERT_EQUAL_INT(0, list.size());
 }
 
+void map_listOfAnimals_listOfPets()
+{
+    std::list<Animal> animalList = {Animal("Zazu", "bird", 9), Animal("Kira", "bird", 9)};
+
+    std::list<Pet> petList = map<Pet, Animal>(animalList, [](const Animal &animal) { return Pet(animal.name, animal.species, animal.age, "Selin"); });
+
+    TEST_ASSERT_EQUAL_STRING("Selin", petList.front().ownerName.c_str());
+    petList.pop_front();
+    TEST_ASSERT_EQUAL_STRING("Selin", petList.front().ownerName.c_str());
+    petList.pop_front();
+    TEST_ASSERT_EQUAL_INT(0, petList.size());
+}
+
 int runUnityTests()
 {
     UNITY_BEGIN();
-    RUN_TEST(map_test);
+    RUN_TEST(map_listOfStrings_listOfSubstrings);
+    RUN_TEST(map_listOfAnimals_listOfPets);
     return UNITY_END();
 }
 
