6ee0c30aea566edc5bfba9977446f0fdb2a78719
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2025-05-25 23:36:47 +0200

    Added more tests for skip

--- test/native/test_skip/skip_tests.cpp
+++ test/native/test_skip/skip_tests.cpp
@@ -32,10 +32,38 @@ void skip_listOfIntegersSkip3_restOfList()
     TEST_ASSERT_EQUAL_INT(0, integers.size());
 }
 
+void skip_listOfAnimalsSkip1_restOfList()
+{
+    LINQish<Animal> animals = { Animal("Kira", "bird", 9), Animal("Sasu", "bird", 9), Animal("Allan", "hamster", 2) };
+
+    auto animalList = animals.skip(1).toList();
+
+    TEST_ASSERT_EQUAL_STRING("Sasu", animalList.front().name.c_str());
+    TEST_ASSERT_EQUAL_STRING("bird", animalList.front().species.c_str());
+    TEST_ASSERT_EQUAL_INT(9, animalList.front().age);
+    animalList.pop_front();
+    TEST_ASSERT_EQUAL_STRING("Allan", animalList.front().name.c_str());
+    TEST_ASSERT_EQUAL_STRING("hamster", animalList.front().species.c_str());
+    TEST_ASSERT_EQUAL_INT(2, animalList.front().age);
+    animalList.pop_front();
+    TEST_ASSERT_EQUAL_INT(0, animalList.size());
+}
+
+void skip_listOfIntegersSkip5_emptyList()
+{
+    LINQish<int> integers = { 1, 2, 3, 4 };
+
+    auto integerList = integers.skip(5).toList();
+
+    TEST_ASSERT_EQUAL_INT(1, integerList.empty());
+}
+
 int runUnityTests()
 {
     UNITY_BEGIN();
     RUN_TEST(skip_listOfIntegersSkip3_restOfList);
+    RUN_TEST(skip_listOfAnimalsSkip1_restOfList);
+    RUN_TEST(skip_listOfIntegersSkip5_emptyList);
     return UNITY_END();
 }
 
