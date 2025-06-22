fad63c667bcae2489dca8bc5294265cb6743332b
NikolajRFA <nrfadk@gmail.com>
2025-06-22 22:27:43 +0200

    Added check_license script and updated workflow

---
+++ .github/scripts/check_license.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+expected_header='Copyright (c) 2025-present Nikolaj Andersen'
+file_extensions=("*.cpp" "*.hpp" "*.h" "*.c")
+missing_files=()
+
+for ext in "${file_extensions[@]}"; do
+  while IFS= read -r -d '' file; do
+    if ! grep -Fq "$expected_header" "$file"; then
+      missing_files+=("$file")
+    fi
+  done < <(find . -type d -name .pio -prune -o -type f -name "$ext" -print0)
+done
+
+if [ ${#missing_files[@]} -gt 0 ]; then
+  echo "The following files are missing the required license header:"
+  for file in "${missing_files[@]}"; do
+    echo "  - $file"
+  done
+  exit 1
+else
+  echo "All checked files include the license header."
+fi
