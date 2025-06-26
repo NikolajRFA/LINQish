dd42330084cf3c34a242fb34a25b4ed39feeec1c
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2025-06-26 20:18:21 +0200

    Migrated takeWhile tests

--- test/embedded/test_takeWhile/takeWhile_tests.cpp
+++ test/embedded/test_takeWhile/takeWhile_tests.cpp
@@ -14,12 +14,17 @@
  limitations under the License.
 **/
 
+
 //
-// Created by nrfad on 09-06-2025.
+// Auto-generated embedded test runner
 //
+#include "Arduino.h"
 #include "../../shared/takeWhile_tests.h"
 
-int main()
+void setup()
 {
-    return runUnityTests();
+    delay(2000);
+    runUnityTests();
 }
+
+void loop() {}
