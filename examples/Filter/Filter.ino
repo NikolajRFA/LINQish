5a6121481b0f1a0db3e96721a7a6d454de1f36cd
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2025-07-15 21:47:34 +0200

    Added simple filter example

--- examples/Filter/Filter.ino
+++ examples/Filter/Filter.ino
@@ -1,11 +1,16 @@
+#include <Arduino.h>
 #include <linqish.h>
 
 void setup()
 {
+    LINQish<int> integers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
+    auto filteredIntegersVector = integers.filter([](const int &x) { return x > 5; }).toVector();
+
+    for (int i = 0; i < filteredIntegersVector.size(); i++)
+    {
+        Serial.println(filteredIntegersVector[i]);
+    }
 }
 
-void loop()
-{
-    
-}
+void loop() {}
